dbcb6dbd985f6348f222504339d2e9fd8908248a
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-09 16:51:55 -0700

    Frontend: Add footer and header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import './App.css'
 import MovieList from './MovieList'
+import Header from './Header'
+import Footer from './Footer'
 
 const App = () => {
 
   return (
     <div className="App">
+        <Header/>
         <MovieList/>
+        <Footer/>
     </div>
   )
 }
---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.css"
+
+const Footer = () => {
+    return (
+        <footer>
+            <p className="footer-text">©2025 Flixter</p>
+        </footer>
+    )
+}
+
+export default Footer;
