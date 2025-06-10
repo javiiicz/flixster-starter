bac0489d58daf6a82c8a856a6ad0a004205cba3e
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-10 15:57:16 -0700

    Frontend: Add placeholder image to movies without poster

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,9 +2,11 @@ import PropTypes from "prop-types";
 import "../styles/MovieCard.css"
 
 const MovieCard = ({title, image, average}) => {
+    let image_src = !(image === null) ? `http://image.tmdb.org/t/p/w500/${image}` : "../../public/placeholder_img.svg"
+
     return (
         <div className="movie-card">
-            <img src={`http://image.tmdb.org/t/p/w500/${image}`} alt={title} className="movie-image"></img>
+            <img src={image_src} alt={title} className="movie-image"></img>
             <h2 className="movie-title">{title}</h2>
             <p className="movie-rating">Rating: {average}</p>
         </div>
@@ -15,6 +17,6 @@ export default MovieCard;
 
 MovieCard.propTypes = {
     title: PropTypes.string.isRequired,
-    image: PropTypes.string.isRequired,
+    image: PropTypes.string,
     average: PropTypes.number.isRequired
 }
