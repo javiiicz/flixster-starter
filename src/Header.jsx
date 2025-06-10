9fc9eb5f74f5e489ed7f12c0f4fb0f980d36b146
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-09 17:00:30 -0700

    Frontend: Add search field and sort field

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,6 +4,20 @@ const Header = () => {
     return (
         <header>
             <h1>🍿 Flixster 🍿</h1>
+
+            
+            <div className="tools">
+                <form className="search-form">
+                    <input type="text"></input>
+                    <button type="submit">Search</button>
+                </form>
+                <select name="sort-options">
+                    <option value="" selected disabled hidden>Sort By</option>
+                    <option value="op1">Option1</option>
+                    <option value="op2">Option2</option>
+                    <option value="op3">Option3</option>
+                </select>
+            </div>
         </header>
     )
 }
