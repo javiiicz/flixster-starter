9e5d034a73f72cf85344ceffbddbd7a67a13d0db
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-09 17:21:59 -0700

    Frontend: Add Load More functionality

--- src/MovieList.jsx
+++ src/MovieList.jsx
@@ -1,34 +1,46 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import MovieCard from "./MovieCard";
 import "./MovieList.css"
 import { parseMovieList } from "./utils/utils";
 
 const MovieList = () => {
     let [movieData, setMovieData] = useState([]);
+    let [currentPage, setCurrentPage] = useState(0)
+
+    useEffect(() => {
+        initialFetchData()
+    }, [])
 
     const apiKey = import.meta.env.VITE_API_KEY;
-    const url = `https://api.themoviedb.org/3/movie/now_playing?&api_key=${apiKey}`;
-    const options = {
-        method: "GET",
-        headers: {
-            accept: 'application/json',
-        }
-    };
-
-    const fetchData = async () => {
-        const response  = await fetch(url, options)
+    
+    const initialFetchData = async () => {
+        const url = `https://api.themoviedb.org/3/movie/now_playing?&api_key=${apiKey}`;
+        const response  = await fetch(url)
         const data = await response.json();
         setMovieData(parseMovieList(data))
+        setCurrentPage(prev => prev + 1)
+        console.log(currentPage)
+
+        // error handling
     }
 
-    fetchData()
-
+    const loadMore = async () => {
+        const url = `https://api.themoviedb.org/3/movie/now_playing?&api_key=${apiKey}&page=${currentPage}`;
+        const response  = await fetch(url)
+        const data = await response.json();
+        setMovieData(prev => prev.concat(parseMovieList(data)))
+        setCurrentPage(prev => prev + 1)
+        console.log(currentPage)
+    }
 
     return (
-        <main className="movie-list-container">
-            {movieData && movieData.map((movie,index) => {
-                return (<MovieCard key={index} title={movie.title} image={movie.image} average={movie.average}/>)
-            })}
+        <main>
+            <div className="movie-list-container">
+                {movieData && movieData.map((movie,index) => {
+                    return (<MovieCard key={index} title={movie.title} image={movie.image} average={movie.average}/>)
+                })}
+            </div>
+            <button onClick={loadMore}>Load More</button>
         </main>
     )
 }
