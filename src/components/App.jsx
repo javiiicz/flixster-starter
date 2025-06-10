bb82101a93075fc5dd149b3fb2138f9ad908dbf8
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-10 12:17:49 -0700

    Frontend: Move data fetching to top component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,35 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import '../styles/App.css'
 import MovieList from './MovieList'
 import Header from './Header'
 import Footer from './Footer'
+import { parseMovieList } from "../utils/utils";
 
 const App = () => {
+    let [movieData, setMovieData] = useState([]);
+    let [currentPage, setCurrentPage] = useState(1)
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+    const apiKey = import.meta.env.VITE_API_KEY;
+    
+    const fetchData = async () => {
+        const url = `https://api.themoviedb.org/3/movie/now_playing?&api_key=${apiKey}&page=${currentPage}`;
+        const response  = await fetch(url)
+        const data = await response.json();
+        setMovieData(prev => prev.concat(parseMovieList(data)))
+        setCurrentPage(prev => prev + 1)
+
+        // error handling
+    }
+
 
   return (
     <div className="App">
         <Header/>
-        <MovieList/>
+        <MovieList loadMore={fetchData} movieData={movieData}/>
         <Footer/>
     </div>
   )
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,37 +1,8 @@
-import { useEffect, useState } from "react";
 import MovieCard from "./MovieCard";
 import "../styles/MovieList.css"
-import { parseMovieList } from "../utils/utils";
 
-const MovieList = () => {
-    let [movieData, setMovieData] = useState([]);
-    let [currentPage, setCurrentPage] = useState(0)
-
-    useEffect(() => {
-        initialFetchData()
-    }, [])
-
-    const apiKey = import.meta.env.VITE_API_KEY;
+const MovieList = ({movieData, loadMore}) => {
     
-    const initialFetchData = async () => {
-        const url = `https://api.themoviedb.org/3/movie/now_playing?&api_key=${apiKey}`;
-        const response  = await fetch(url)
-        const data = await response.json();
-        setMovieData(parseMovieList(data))
-        setCurrentPage(prev => prev + 1)
-        console.log(currentPage)
-
-        // error handling
-    }
-
-    const loadMore = async () => {
-        const url = `https://api.themoviedb.org/3/movie/now_playing?&api_key=${apiKey}&page=${currentPage}`;
-        const response  = await fetch(url)
-        const data = await response.json();
-        setMovieData(prev => prev.concat(parseMovieList(data)))
-        setCurrentPage(prev => prev + 1)
-        console.log(currentPage)
-    }
 
     return (
         <main>
