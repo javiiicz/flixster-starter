6c828eba46b8b766fa42ddf75762f3378b04dc3c
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-11 15:29:10 -0700

    [Stretch] Add placeholder video

--- src/components/MovieModal.jsx
+++ src/components/MovieModal.jsx
@@ -1,9 +1,20 @@
+import { useState } from "react";
 import "../styles/MovieModal.css";
 
 const MovieModal = ({ setShowModal, movie }) => {
+    const [showTrailer, setShowTrailer] = useState(false)
+
     const handleClickInside = (e) => {
         e.stopPropagation();
     };
+
+    const toggleTrailer = () => {
+        if (showTrailer) {
+            setShowTrailer(false)
+        } else {
+            setShowTrailer(true)
+        }
+    }
 
     return (
         <div
@@ -14,16 +25,28 @@ const MovieModal = ({ setShowModal, movie }) => {
         >
             <div className="modal-content" onClick={handleClickInside}>
                 <h2>{movie ? movie.title : "Movie Title"}</h2>
-                <div className="poster-container">
-                    <div className="backdrop-image">
-                        <img
-                            src={
-                                movie.image
-                                    ? `http://image.tmdb.org/t/p/w780/${movie.image}`
-                                    : "../../public/placeholder_img.svg"
-                            }
-                            alt={movie.title}
-                        ></img>
+                <div className="movie-container">
+                    <div className="media-container">
+                        <div className="slider-frame">
+                            <div className={showTrailer ? "slider slide2" : "slider"}>
+                                <img
+                                    src={
+                                        movie.image
+                                            ? `http://image.tmdb.org/t/p/w780/${movie.image}`
+                                            : "../../public/placeholder_img.svg"
+                                    }
+                                    alt={movie.title}
+                                ></img>
+                                <iframe
+                                    className="video-player"
+                                    src="https://www.youtube.com/embed/lJIrF4YjHfQ?si=BBzq24PhefFbBPLB"
+                                    title="YouTube video player"
+                                    frameborder="0"
+                                    allowfullscreen
+                                ></iframe>
+                            </div>
+                        </div>
+                        <button onClick={toggleTrailer}>Toggle Backdrop / Trailer</button>
                     </div>
                     <div className="details-container">
                         <p>
