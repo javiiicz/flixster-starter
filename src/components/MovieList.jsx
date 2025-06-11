a6281de27604fead30e8dc1c856cb93cd4821a24
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-11 14:34:01 -0700

    [MVP] Refactor Movie List code

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -2,32 +2,36 @@ import MovieCard from "./MovieCard";
 import "../styles/MovieList.css";
 import { useEffect } from "react";
 
-const MovieList = ({ movieData, loadMore, handleMovieClick}) => {
-    return (
-        <main>
-            {!(movieData.length === 0) ? (
-                <>
-                    <div className="movie-list-container">
-                        {movieData.map((movie, index) => {
-                            return (
-                                <MovieCard
-                                    key={index}
-                                    movie_id={movie.id}
-                                    title={movie.title}
-                                    image={movie.image}
-                                    average={movie.average}
-                                    handleMovieClick={handleMovieClick}
-                                />
-                            );
-                        })}
-                    </div>
-                    <button onClick={loadMore}>Load More</button>
-                </>
-            ) : (
+const MovieList = ({ movieData, loadMore, handleMovieClick }) => {
+    if (movieData.length === 0) {
+        return (
+            <main>
                 <div className="nothing">
                     <h2>Nothing to show...</h2>
                 </div>
-            )}
+            </main>
+        );
+    }
+
+    
+
+    return (
+        <main>
+            <div className="movie-list-container">
+                {movieData.map((movie, index) => {
+                    return (
+                        <MovieCard
+                            key={index}
+                            movie_id={movie.id}
+                            title={movie.title}
+                            image={movie.image}
+                            average={movie.average}
+                            handleMovieClick={handleMovieClick}
+                        />
+                    );
+                })}
+            </div>
+            <button onClick={loadMore}>Load More</button>
         </main>
     );
 };
