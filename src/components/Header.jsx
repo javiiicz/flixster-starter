9f80b8e524e05fb957a4aceed29db8e4eb85760c
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-12 12:09:34 -0700

    [Stretch] Style sidebar button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,48 @@
-import "../styles/Header.css"
+import "../styles/Header.css";
 import NavTools from "./NavTools";
 
-const Header = ({handleTextChange, handleSearchSubmit, showSearch, setShowSearch, clearSearch, setSortOption}) => {
-    const nowTabClass = showSearch ? "tab" : "tab active"
-    const searchTabClass = showSearch ? "tab active" : "tab"
+const Header = ({
+    handleTextChange,
+    handleSearchSubmit,
+    showSearch,
+    setShowSearch,
+    clearSearch,
+    setSortOption,
+    openSidebar,
+    setOpenSidebar,
+}) => {
+    const nowTabClass = showSearch ? "tab" : "tab active";
+    const searchTabClass = showSearch ? "tab active" : "tab";
 
     return (
         <header>
             <div className="header-container">
                 <h1>🍿 Flixster 🍿</h1>
-                <NavTools handleTextChange={handleTextChange} handleSearchSubmit={handleSearchSubmit} clearSearch={clearSearch} setSortOption={setSortOption}/>
+                <NavTools
+                    handleTextChange={handleTextChange}
+                    handleSearchSubmit={handleSearchSubmit}
+                    clearSearch={clearSearch}
+                    setSortOption={setSortOption}
+                    openSidebar={openSidebar}
+                    setOpenSidebar={setOpenSidebar}
+                />
             </div>
 
             <nav className="view-tabs">
-                <button className={nowTabClass} onClick={() => {setShowSearch(false)}}>Now Playing</button>
-                <button className={searchTabClass} onClick={handleSearchSubmit}>Search</button>
+                <button
+                    className={nowTabClass}
+                    onClick={() => {
+                        setShowSearch(false);
+                    }}
+                >
+                    Now Playing
+                </button>
+                <button className={searchTabClass} onClick={handleSearchSubmit}>
+                    Search
+                </button>
             </nav>
         </header>
-    )
-}
+    );
+};
 
 export default Header;
--- src/components/NavTools.jsx
+++ src/components/NavTools.jsx
@@ -1,21 +1,51 @@
 import "../styles/NavTools.css";
 
-const NavTools = ({handleTextChange, handleSearchSubmit, clearSearch, setSortOption}) => {
+const NavTools = ({
+    handleTextChange,
+    handleSearchSubmit,
+    clearSearch,
+    setSortOption,
+    openSidebar,
+    setOpenSidebar,
+}) => {
+    const toggleSidebar = () => {
+        setOpenSidebar(!openSidebar)
+    }
+
     return (
         <div className="tools">
-            <form className="search-form" onSubmit={handleSearchSubmit} id='search-form'>
+            <div
+                className={
+                    openSidebar ? "sidebar-container open" : "sidebar-container"
+                }
+                onClick={toggleSidebar}
+            >
+                <img
+                    width="100%"
+                    className="sidebar-img"
+                    src="./arrow-right.svg"
+                    alt="Open Sidebar"
+                ></img>
+            </div>
+            <form
+                className="search-form"
+                onSubmit={handleSearchSubmit}
+                id="search-form"
+            >
                 <input type="text" onChange={handleTextChange}></input>
                 <div className="buttons">
                     <button type="submit">Search</button>
                     <button onClick={clearSearch}>Clear</button>
                 </div>
             </form>
-            <select name="sort-options" defaultValue="" onChange={(e) => {
-                setSortOption(e.target.value)
-            }}>
-                <option value="">
-                    Sort By
-                </option>
+            <select
+                name="sort-options"
+                defaultValue=""
+                onChange={(e) => {
+                    setSortOption(e.target.value);
+                }}
+            >
+                <option value="">Sort By</option>
                 <option value="title">Title</option>
                 <option value="date">Release date</option>
                 <option value="average">Vote average</option>
