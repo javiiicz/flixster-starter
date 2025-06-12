66161900c9257443b84e8e00c460af9b36520785
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-12 16:17:50 -0700

    [Stretch] [Styling] Add Sidebar link styling

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,15 +1,36 @@
-import "../styles/Sidebar.css"
+import "../styles/Sidebar.css";
 
-const Sidebar = () => {
+const Sidebar = ({ route, setRoute }) => {
     return (
-            <div className="sidebar-content">
-                <ul className="page-list">
-                    <li>Home</li>
-                    <li>Liked</li>
-                    <li>Watched</li>
-                </ul>
-            </div>
-    )
-}
+        <div className="sidebar-content">
+            <ul className="page-list">
+                <li
+                    className={(route === "nowPlaying" || route === "search") ? "selected" : ""}
+                    onClick={() => {
+                        setRoute("nowPlaying");
+                    }}
+                >
+                    Home
+                </li>
+                <li
+                    className={route === "liked" ? "selected" : ""}
+                    onClick={() => {
+                        setRoute("liked");
+                    }}
+                >
+                    Liked
+                </li>
+                <li
+                    className={route === "watched" ? "selected" : ""}
+                    onClick={() => {
+                        setRoute("watched");
+                    }}
+                >
+                    Watched
+                </li>
+            </ul>
+        </div>
+    );
+};
 
 export default Sidebar;
