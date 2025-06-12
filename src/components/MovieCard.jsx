0c7bcfbb04b0245f2f3c21c097adb875bcaebe62
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-12 10:07:48 -0700

    [Stretch] Add and style heart and watch buttons

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,6 +7,10 @@ const MovieCard = ({movie_id, title, image, average, handleMovieClick}) => {
     return (
         <article className="movie-card" onClick={() => {handleMovieClick(movie_id)}}>
             <img src={image_src} alt={`${title} Movie Poster`} className="movie-image"></img>
+            <div className="user-buttons">
+                <div className="icon-container"><img src="./heart.svg" className="icon heart" alt="Add to Liked"></img></div>
+                <div className="icon-container"><img src="./eye.svg" className="icon eye" alt="Add to Watched"></img></div>
+            </div>
             <h3 className="movie-title">{title}</h3>
             <p className="movie-rating">Rating: {average}</p>
         </article>
